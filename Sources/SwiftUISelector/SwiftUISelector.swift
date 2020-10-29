32d0550c20397b9be531d593c7133a9bd80a44c4
Peter Larson <p.larson@utexas.edu>
2020-10-29 09:49:05 -0500

    Initial Commit

--- Sources/SwiftUISelector/SwiftUISelector.swift
+++ Sources/SwiftUISelector/SwiftUISelector.swift
@@ -1,3 +1,70 @@
-struct SwiftUISelector {
-    var text = "Hello, World!"
+import SwiftUI
+
+public class SelectionsModel: ObservableObject {
+    @Published public var selectionIndex: Int
+    @Published public var options: [String]
+    
+    public init(_ selectionIndex: Int = 0, options: [String]) {
+        self.selectionIndex = selectionIndex
+        self.options = options
+    }
+}
+
+public struct SelectorView: View {
+    
+    @Namespace private var environment
+    
+    @ObservedObject private var model: SelectionsModel
+    
+    public init(model: SelectionsModel) {
+        self.model = model
+    }
+        
+    public var body: some View {
+        VStack {
+            ScrollView(.horizontal, showsIndicators: false) {
+                ScrollViewReader { scrollProxy in
+                    HStack(spacing: 40) {
+                        ForEach(0 ..< self.model.options.count) { option in
+                            Text(self.model.options[option])
+                                .matchedGeometryEffect(id: option, in: environment, isSource: true)
+                                .onTapGesture {
+                                    withAnimation(.easeInOut) {
+                                        
+                                        self.model.selectionIndex = option
+                                        
+                                        scrollProxy.scrollTo(option, anchor: .center)
+                                    }
+                                }
+                        }
+                        .fixedSize()
+                        .alignmentGuide(HorizontalAlignment.center, computeValue: { dimension in
+                            dimension[.leading]
+                        })
+                    }
+                    .font(
+                        .system(
+                            size: 24,
+                            weight: Font.Weight.medium,
+                            design: Font.Design.serif
+                        )
+                    )
+                    .onReceive(self.model.$selectionIndex, perform: { _ in
+                        scrollProxy.scrollTo(self.model.selectionIndex, anchor: .center)
+                    })
+                }
+                .padding()
+            }
+            
+            Capsule()
+                .frame(height: 1.5)
+                .matchedGeometryEffect(
+                    id: self.model.selectionIndex,
+                    in: environment,
+                    properties: MatchedGeometryProperties.frame,
+                    isSource: false
+                )
+                .foregroundColor(.accentColor)
+        }
+    }
 }
--- Tests/SwiftUISelectorTests/SwiftUISelectorTests.swift
+++ Tests/SwiftUISelectorTests/SwiftUISelectorTests.swift
@@ -1,12 +1,12 @@
 import XCTest
-@testable import SwiftUISelector
+//@testable import SwiftUISelector
 
 final class SwiftUISelectorTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftUISelector().text, "Hello, World!")
+        // XCTAssertEqual(SwiftUISelector(), "Hello, World!")
     }
 
     static var allTests = [
